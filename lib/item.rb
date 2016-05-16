2a71c888b7ecc9d79ff6fc1b6abb649a69fae23a
Zoltan Biber <zoltan.biber@gmail.com>
2016-05-17 00:24:25 +0100

    Item and ShoppingBasket classes ready.

---
+++ lib/item.rb
@@ -0,0 +1,9 @@
+class Item
+	attr_accessor :code, :name, :price
+
+	def initialize(code, name, price)
+		@code = code
+		@name = name
+		@price = price
+	end
+end
---
+++ lib/shopping_basket.rb
@@ -0,0 +1,7 @@
+class ShoppingBasket
+	attr_reader :items
+
+	def initialize(*items)
+		@items = *items
+	end
+end
